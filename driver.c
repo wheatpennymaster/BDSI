4773b531e7a6c9981687ad63ca11d6aa51a351c7
Thomas Tavolara <ttavolar@u.rochester.edu>
2014-05-26 15:45:22 -0400

    working on delimiting raw w303 string
    delimiting works

--- driver.c
+++ driver.c
@@ -8,20 +8,7 @@ struct chr
 	char * seq;
 };
 
-char * fasta_concat(char * s1, char * s2)
-{
-	char * con;
-	con = (char *)malloc(strlen(s1) +strlen(s2)+1);
-	int i=0;
-	for (i=0; i<strlen(s1); i++)
-		con[i]=s1[i];
-	for (int j=0; j<strlen(s2); j++, i++)
-		con[i]=s2[j];
-	con[i]='\0';
-	return con;
-}
-
-char * read_fasta(void)
+char * read_file(void)
 {
 	char * file_contents;
 	long input_file_size;
@@ -35,10 +22,71 @@ char * read_fasta(void)
 	return file_contents;
 }
 
+void n(void)
+{
+	printf("\n");
+}
+
+struct chr get_gene(char * s)
+{
+	struct chr gene;
+	char * pch;
+	pch = strtok(s,"\n");
+	int c = 0;
+	while(pch !=NULL)
+	{
+		printf("%i: %s\n",c++,pch);
+		if(pch[0]=='>')
+		{
+			gene.chr_name=pch;
+			pch = strtok(NULL,"\n");
+		}
+		
+		/*
+		char * seq = gene.seq;
+		gene.seq = malloc((strlen(seq) + strlen(pch)) * sizeof(char));
+		int i;
+		for(i=0;i<strlen(seq);i++)
+			gene.seq[i]=seq[i];
+		for(int j=0;j<strlen(pch);j++,i++)
+			gene.seq[i]=pch[j];
+		gene.seq[i]='\0';
+		
+		*/
+		pch = strtok(NULL,"\n");
+		if(strcmp(pch,">chrII")==0)
+			break;
+	}
+
+	return gene;
+}
+
 
 int main()
 {
-	char * w303_file = read_fasta();
-	printf("%s\n",w303_file);
+	char * w303 = read_file();
+	struct chr test = get_gene(w303);
 	
+	/*
+	printf("%lu\n",strlen(w303));
+	
+	struct chr test[10];
+	test[0].chr_name="myname";
+	test[0].seq="AA";
+	
+
+	for(int t=0;t<5;t++)
+	{
+		char * seq = test[0].seq;
+		char * seq1 = "TT";
+	printf("Name: %s\nSequence: %s\n",test[0].chr_name,test[0].seq);
+	test[0].seq = malloc((strlen(seq) + strlen(seq1)) * sizeof(char));
+	int i;
+	for(i=0;i<strlen(seq);i++)
+		test[0].seq[i]=seq[i];
+	for(int j=0;j<strlen(seq1);j++,i++)
+		test[0].seq[i]=seq1[j];
+	test[0].seq[i]='\0';
+	}
+	*/
 }
