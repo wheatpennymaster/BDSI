ffc88fb3af786ef305ef4ea8085667c0a7742b16
Thomas Tavolara <ttavolar@u.rochester.edu>
2014-05-26 16:13:43 -0400

    parsing for one gene works

--- driver.c
+++ driver.c
@@ -45,12 +45,8 @@ struct chr get_gene(char * s)
 		
 		char * seq = gene.seq;
 		gene.seq = malloc((strlen(seq) + strlen(pch)) * sizeof(char));
-		int i;
-		for(i=0;i<strlen(seq);i++)
-			gene.seq[i]=seq[i];
-		for(int j=0;j<strlen(pch);j++,i++)
-			gene.seq[i]=pch[j];
-		gene.seq[i]='\0';
+		strcpy(gene.seq,seq);
+		strncat(gene.seq,pch,strlen(pch));
 		
 		
 		pch = strtok(NULL,"\n");
@@ -65,32 +61,8 @@ struct chr get_gene(char * s)
 int main()
 {
 	char * w303 = read_file();
-	//struct chr test = get_gene(w303);
-	
-	
-	printf("%lu\n",strlen(w303));
-	
-	struct chr test[10];
-	test[0].chr_name="myname";
-	test[0].seq="AA";
+	struct chr test = get_gene(w303);
+	printf("Name: %s\nSequence: %s\n",test.chr_name,test.seq);
 	
 
-	for(int t=0;t<5;t++)
-	{
-		char * seq = test[0].seq;
-		char * seq1 = "TT";
-	printf("Name: %s\nSequence: %s\n",test[0].chr_name,test[0].seq);
-	test[0].seq = malloc((strlen(seq) + strlen(seq1)) * sizeof(char));
-	int i;
-	for(i=0;i<strlen(seq);i++)
-		test[0].seq[i]=seq[i];
-	for(int j=0;j<strlen(seq1);j++,i++)
-		test[0].seq[i]=seq1[j];
-	test[0].seq[i]='\0';
-	}
-	n();n();
-	
-	
-	
-	
 }
